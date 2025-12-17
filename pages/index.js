657e17220d3eb31a89f66bceaa4eb7edbdd75968
f8902621-byte <f8902621@gmail.com>
2025-12-17 14:11:45 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -142,7 +142,7 @@ export default function Home() {
     setShowSearch(false);
 
     try {
-      const response = await fetch('/api/search', {
+      const response = await fetch('/.netlify/functions/search', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(searchParams)
